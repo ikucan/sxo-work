dafb5ae140a8a308da2e5534418f7ce577a85b6e
ikucan <bitch@gmail.com>
2023-04-19 21:31:02 +0100

    ...

--- notebooks/strats/utils.py
+++ notebooks/strats/utils.py
@@ -3,5 +3,6 @@ import pandas as pd
 class Binner:
 
     @staticmethod
-    def add_bin_col(df:pd.DataFrame, bind_size_min:int):
+    def add_bin_col(df:pd.DataFrame, bind_size_sec:int, timestamp_col:str, bin_time_col:str):
+        
         pass
