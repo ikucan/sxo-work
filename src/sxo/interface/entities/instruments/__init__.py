2fc5f00f2b04e52f7a6994e84b27ead2b327b8db
ikucan <bitch@gmail.com>
2023-04-25 20:58:51 +0100

    .

--- src/sxo/interface/entities/instruments/__init__.py
+++ src/sxo/interface/entities/instruments/__init__.py
@@ -2,9 +2,9 @@
 from pathlib import Path
 
 import sxo.interface.entities.instruments as ref_db
-from sxo.interface.entities.instruments.reference import FxSpotDb
-from sxo.interface.entities.instruments.reference import EquityDb
+from sxo.interface.entities.instruments.reference import FxSpotSyms
+from sxo.interface.entities.instruments.reference import EquitySyms
 
 # check the path - these need to be colocated (or modify path)
-FxSpotInstruments = FxSpotDb(Path(ref_db.__file__).parent / "FxSpot.json")
-EquityInstruments = EquityDb(Path(ref_db.__file__).parent / "Stock.json")
+FxSpotInstruments = FxSpotSyms(Path(ref_db.__file__).parent / "FxSpot.json")
+EquityInstruments = EquitySyms(Path(ref_db.__file__).parent / "Stock.json")
--- src/sxo/interface/entities/instruments/entities.py
+++ src/sxo/interface/entities/instruments/entities.py
@@ -8,107 +8,37 @@ from typing import List
 from typing import Set
 from typing import Union
 
-class JsonDataBase:
-    def __init__(self, asset_type: str, _json: Union[Dict, str, Path, None]):
-        if asset_type is None:
-            raise ValueError("you must pass a valid asset_type parameter")
+class Instrument(ABC):
+    @staticmethod
+    def parse(sym:str):
+        try:
+            assetClass, symbol = sym.split("::")
+        except ValueError as ve:
+            raise Exception("Error parsing instrument. Expecting string in form of <asset class>::<symbol>")
+        #if assetClass is None:
 
-        self._asset_type = asset_type
-        # if none, look for the asset class relevant json db
-        if _json is None:
-            _json = Path(__file__).parent / f"{self._asset_type}.json"
-            if not _json.exists():
-                raise ValueError("No asssed db json passed. Using default {self._asset_type}.json did not work.")
-
-        if isinstance(_json, Dict):
-            self._data = _json["Data"]
-        elif isinstance(_json, str):
-            self._data = json.loads(_json)["Data"]
-        elif isinstance(_json, Path):
-            self._data = json.load(open(_json.as_posix()))["Data"]
-        else:
-            raise ValueError(
-                "parameter _json needs to be either a" " parsed json dict, a Path to file or a" " string containing unparsed json"
-            )
-
-        self._by_symbol = {x["Symbol"]: x for x in self._data}
-        self._all_symbols = set(self._by_symbol.keys())
-        asset_types = {x["AssetType"] for x in self._data}
-        if len(asset_types) != 1:
-            raise ValueError(f"expected single asset type, got : {asset_types}")
-        if self._asset_type not in asset_types:
-            raise ValueError(f"expected {self._asset_type} as the asset type, got : {asset_types}")
-
-    def get_asset_type(
-        self,
-    ) -> str:
-        return self._asset_type
-
-    def all_symbols(
-        self,
-    ) -> Set[str]:
-        return self._all_symbols
-
-    def has_pair(
-        self,
-        pair: str,
-    ) -> bool:
-        return pair in self._all_symbols
-
-    def get_instrument(
-        self,
-        instrument: str,
-    ) -> Dict:
-        if instrument not in self._by_symbol:
-            raise ValueError(f"unknown pair: {instrument}")
-        return self._by_symbol[instrument]
-
-    def get_instrument_id(
-        self,
-        symbol: Union[str, List[str]],
-    ) -> Union[int, List]:
-        if isinstance(symbol, List):
-            return [self.get_instrument_id(p) for p in symbol]
-
-        instrument = self.get_instrument(symbol)
-        if "Identifier" not in instrument:
-            raise ValueError(f"unknown instrument: {symbol}")
-        return instrument["Identifier"]
-
-
-class SaxoAssetBase(ABC):    
+    
     @abstractmethod
-    def get_asset_class():
+    def make_from_symbol():
         pass
 
-
-# TODO:>> ik:>> make singleton, ensure thread safety
-# from singleton_decorator import singleton
-# @singleton
-class FxSpot(SaxoAssetBase,JsonDataBase):
+class FxSpot(Instrument):
     """
     a wrapper for the FxSpot entity reference data
     """
-
-    def __init__(self, _json: Union[Dict, str, Path, None] = None):
-        super().__init__(asset_type="FxSpot", _json=_json)
-
-
+#
     def get_asset_class():
         return "xxx"
 
 # EQUITY symbology
-class Equity(SaxoAssetBase, JsonDataBase):
-    """
-    a wrapper for the FxSpot entity reference data
-    """
-
-    def __init__(self, _json: Union[Dict, str, Path, None] = None):
-        super().__init__(asset_type="Stock", _json=_json)
+class Equity(Instrument):
 
     def get_asset_class():
         return "xxx"
 
 
 if __name__ == "__main__":
-    print(123)
+    for sym in ["Fx::USDGBP", ""]:
+        s = Instrument.parse(sym)
+        print(s)
+
