fb20c4ac2f3191bf78716407850b811da4f55886
ikucan <bitch@gmail.com>
2023-06-15 19:27:47 +0100

    ...

--- src/sxo/apps/simple/run.sh
+++ src/sxo/apps/simple/run.sh
@@ -2,4 +2,6 @@ export PYTHONPATH="/workstem/sxo-ifc/src"
 export TOKEN_FILE="/tmp/saxo_token"
 export INSTRUMENTS="FxSpot::GBPEUR"
 export DATA_DIR="/data"
-python simple_strategy.py
+export REDIS_PASS="boss"
+ 
+python mainline.py
