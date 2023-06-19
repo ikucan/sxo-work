7ab8faf700e3f1d43c83686c510b024a6b9d6719
ikucan <bitch@gmail.com>
2023-06-19 20:20:19 +0100

    binning of data completed

--- src/sxo/apps/simple/run.sh
+++ src/sxo/apps/simple/run.sh
@@ -1,15 +1,10 @@
 export PYTHONPATH="/workstem/sxo-ifc/src"
-export TOKEN_FILE="/tmp/saxo_token"
+export TOKEN_FILE="/data/saxo_token"
 export INSTRUMENTS="FxSpot::GBPEUR"
 export DATA_DIR="/data"
 export REDIS_PASS="boss"
-<<<<<<< HEAD
 
 while [ 1 -lt 2 ]; do 
     python mainline.py
     sleep 5
 done
-=======
- 
-python mainline.py
->>>>>>> fb20c4ac2f3191bf78716407850b811da4f55886
