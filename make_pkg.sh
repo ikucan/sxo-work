4a309e0412ccd06d31a96c8de29177fd35199760
ikucan <bitch@gmail.com>
2023-10-09 20:21:39 +0100

    moved packaging basis to python 3.11.6

--- make_pkg.sh
+++ make_pkg.sh
@@ -6,4 +6,8 @@ rm -Rf build/
 #
 # build conda package
 #
-conda run -n mamba-build conda mambabuild --python=3.10 --no-remove-work-dir packaging/conda --output-folder build
+# chose the target environement: needs conda-build, setuptools and boa (for mambabuild)
+#
+conda run -n mamba-build2 conda mambabuild --python=3.11 --no-remove-work-dir packaging/conda --output-folder build
+#
+#
