c9b54232bbd81e90dc9508733505ac14b2841fa1
ikucan <bitch@gmail.com>
2023-10-17 18:28:49 +0100

    add a sxo base image

--- make_pkg_and_docker.sh
+++ make_pkg_and_docker.sh
@@ -6,17 +6,19 @@ rm -Rf build/
 #
 # build conda package
 #
-conda run -n mamba-build conda mambabuild --python=3.10 --no-remove-work-dir packaging/conda --output-folder build
+conda run -n mamba-build2 conda mambabuild --python=3.11 --no-remove-work-dir packaging/conda --output-folder build
+
 #
 # copy the binary and the repo information to target directory
 #
-export TARGET_DOCKER_DIR=packaging/docker/targets/data-feeds/
+export TARGET_DOCKER_DIR=packaging/docker/py/sxo-base
+export PACK_VER="v0.2.0-py311_0"
 #
-rm -f $TARGET_DOCKER_DIR/ik.sxo-v0.1.0-py311_0.tar.bz2
+rm -f $TARGET_DOCKER_DIR/ik.sxo-${PACK_VER}.tar.bz2
 rm -f $TARGET_DOCKER_DIR/repodata-linux-64.json
 rm -f $TARGET_DOCKER_DIR/repodata-noarch.json
 #
-cp build/linux-64/ik.sxo-v0.1.0-py311_0.tar.bz2 $TARGET_DOCKER_DIR
+cp build/linux-64/ik.sxo-${PACK_VER}.tar.bz2 $TARGET_DOCKER_DIR
 cp build/linux-64/repodata.json $TARGET_DOCKER_DIR/repodata-linux-64.json
 cp build/noarch/repodata.json $TARGET_DOCKER_DIR/repodata-noarch.json
 #
