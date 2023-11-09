51c3eacc0832f8b035f1b2520e5781df2eb93281
ikucan <bitch@gmail.com>
2023-11-09 07:36:57 +0000

    added more order parsing, order type enumbs

--- src/sxo/interface/entities/om/orders.py
+++ src/sxo/interface/entities/om/orders.py
@@ -13,6 +13,19 @@ class OrderError(Exception):
     ...
 
 
+class Color(Enum):
+    RED = 1
+    GREEN = 2
+    BLUE = 3
+    
+class OrderDuration:
+
+
+            # "Duration": {
+            #     "DurationType": "GoodTillCancel"
+            # },
+
+
 class Order(JsonWrapperBase):
 
     '''
@@ -33,6 +46,53 @@ class Order(JsonWrapperBase):
         self.set_str('ClientKey')
         self.set_str('ClientName')
         self.set_str('ClientNote')
+        self.set_str('CorrelationKey')
+        self.must_have('CorrelationTypes')
+        self.set_float('CurrentPrice')
+        self.set_float('CurrentPriceDelayMinutes')
+        self.set_str('CurrentPriceType')
+        self.set_float('DistanceToMarket')
+        #
+        # parse nested
+        # 
+        self.set_float('IpoSubscriptionFee')
+        self.set_bool('IsExtendedHoursEnabled')
+        self.set_bool('IsForceOpen')
+        self.set_bool('IsMarketOpen')
+        self.set_float('MarketPrice')
+        self.set_str('MarketState')
+        self.set_float('MarketValue')
+        self.set_str('NonTradableReason')
+        self.set_str('OpenOrderType')
+        self.set_str('OrderAmountType')
+        self.set_int('OrderId')
+        self.set_str('OrderRelation')
+        self.set_timestamp('OrderTime')
+        self.set_float('Price')
+        self.set_str('RelatedOpenOrders')
+        self.set_int('RelatedPositionId')
+        self.set_str('Status')
+        self.set_str('TradingStatus')
+        self.set_int('Uic')
+
+
+            # "DisplayAndFormat": {
+            #     "Currency": "EUR",
+            #     "Decimals": 4,
+            #     "Description": "British Pound/Euro",
+            #     "Format": "AllowDecimalPips",
+            #     "Symbol": "GBPEUR"
+            # },
+            # "Duration": {
+            #     "DurationType": "GoodTillCancel"
+            # },
+            # "Exchange": {
+            #     "Description": "Inter Bank",
+            #     "ExchangeId": "SBFX",
+            #     "IsOpen": true,
+            #     "TimeZoneId": "3"
+            # },
+  
 
     def __str__(self,) -> str:
         return pprint(self._json)
@@ -45,7 +105,6 @@ class Order(JsonWrapperBase):
             raise OrderError('ERROR, missing key in initial JSON: Data')
         all_orders = [Order(oj) for oj in _json['Data']]
 
-        i = 123
         pass
 
 import json
