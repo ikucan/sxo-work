8fb63a55b5104a6a034b94a2db046e6ca9c5b41b
iztok <you@example.com>
2023-11-22 07:00:16 +0000

    initial NOOP commit

---
+++ test/sxo/positions/test_position_streaming.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import json
+from pprint import pprint
+import time
+from sxo.interface.entities.om.positions import *
+from sxo.interface.client import SaxoClient
+
+
+if __name__ == "__main__":
+    print(123)
+    # # client = SaxoClient(token_file="/data/saxo_token")
+    # # positions = client.all_positions()
+    # f=open('samples/positions/net_pos_example.json', 'r')
+    # positions_str = f.read()
+    # positions = json.loads(positions_str)
+    # f.close()
+    # #pprint(positions)
+    # np = NetPosition.parse(positions)
+    # for k,v in np.items():
+    #     pprint(v.toJson())
+
+
