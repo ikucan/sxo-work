63e7edf9c04849e7b01fdb3b6209954fa68d28ce
iztok <you@example.com>
2023-12-20 22:21:14 +0000

    ...

--- packaging/docker/targets/clickhouse/csv_import.sh
+++ packaging/docker/targets/clickhouse/csv_import.sh
@@ -3,14 +3,15 @@
 export DATA_DIR="/data/capture.copy"
 export DB_NAME="Saxo"
 export ASSET_CLASS="FxSpot"
+export FQ_TAB="${DB_NAME}.${ASSET_CLASS}"
 
 export CH_HOST=192.168.0.201
 
 export CH_PORT=9000
 
-docker run --rm -it --net=host bitnami/clickhouse:23 clickhouse-client --host=${CH_HOST} --port=${CH_PORT} -q "CREATE DATABASE IF NOT EXISTS Saxo"
-docker run --rm -it --net=host bitnami/clickhouse:23 clickhouse-client --host=${CH_HOST} --port=${CH_PORT} -q "DROP TABLE IF EXISTS Saxo.FxQuotes"
-docker run --rm -it --net=host bitnami/clickhouse:23 clickhouse-client --host=${CH_HOST} --port=${CH_PORT} -q "CREATE TABLE Saxo.FxQuotes (d date, t DateTime64(6), pair char(6), bid Float64, bsz Float64, ask Float64, asz Float64) ENGINE = MergeTree() ORDER BY (d, t, pair)"
+docker run --rm -it --net=host bitnami/clickhouse:23 clickhouse-client --host=${CH_HOST} --port=${CH_PORT} -q "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
+docker run --rm -it --net=host bitnami/clickhouse:23 clickhouse-client --host=${CH_HOST} --port=${CH_PORT} -q "DROP TABLE IF EXISTS ${FQ_TAB}"
+docker run --rm -it --net=host bitnami/clickhouse:23 clickhouse-client --host=${CH_HOST} --port=${CH_PORT} -q "CREATE TABLE ${FQ_TAB} (d date, t DateTime64(6), pair char(6), bid Float64, bsz Float64, ask Float64, asz Float64) ENGINE = MergeTree() ORDER BY (d, t, pair)"
 
 for p in `ls ${DATA_DIR}/${ASSET_CLASS}`; do
     echo === ${p} ===
@@ -18,6 +19,8 @@ for p in `ls ${DATA_DIR}/${ASSET_CLASS}`; do
     echo "    pair dir: ${PAIR_DIR}"
     pushd $PAIR_DIR
     cat $p-2*.csv | gawk -F\, -v PAIR=$p -v q=\" '{print q substr($1,0,10) q "," q $1 q "," q PAIR q "," $2 "," $3 "," $4 "," $5}' | sed "s/Z\"\,/\"\,/" > /tmp/$p.csv
-    docker run --rm -it -v /tmp/$p.csv/:/data/$p.csv --net=host bitnami/clickhouse:23 clickhouse-client --host=${CH_HOST} --port=${CH_PORT} -q "insert into Saxo.FxQuotes FROM INFILE '/data/$p.csv' FORMAT CSV"
+    docker run --rm -it -v /tmp/$p.csv/:/data/$p.csv --net=host bitnami/clickhouse:23 clickhouse-client --host=${CH_HOST} --port=${CH_PORT} -q "insert into ${FQ_TAB} FROM INFILE '/data/$p.csv' FORMAT CSV"
 done
-34
+
+echo "" 
+echo " === DONE ===" 
